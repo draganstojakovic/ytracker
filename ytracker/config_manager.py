dd6ad2927a8a436f5249aa5e773ba741424c887e
Dragan Stojakovic <stojakovicdragan98@gmail.com>
2023-08-23 22:03:08 +0200

    add storage_size option

--- ytracker/config_manager.py
+++ ytracker/config_manager.py
@@ -4,10 +4,12 @@ from global_constants import PACKAGE_NAME
 
 
 class _Options:
-    def __init__(self, /, download_path=None, split_by_channel=None, refresh_interval=None):
+
+    def __init__(self, /, download_path=None, split_by_channel=None, refresh_interval=None, storage_size=None):
         self._set_download_path(download_path)
         self._set_split_by_channel(split_by_channel)
         self._set_refresh_interval(refresh_interval)
+        self._set_storage_size(storage_size)
 
     def _set_download_path(self, download_path: str | None) -> None:
         if download_path is not None:
@@ -32,9 +34,30 @@ class _Options:
         else:
             self.refresh_interval = 2
 
+    def _set_storage_size(self, storage_size: int | float | str | None) -> None:
+        """
+
+        :param storage_size: is maximum amount of gigabytes of storage videos can take
+        """
+        if storage_size is None:
+            self.storage_size = 5
+            return
+        if storage_size is str:
+            self.storage_size = int(float(storage_size))
+            return
+        self.storage_size = int(storage_size)
+
 
 class Config:
+
     options: _Options
+
+    _DEFAULT_CONFIG = {
+        'download_path': os.path.join(os.environ.get('HOME'), 'Videos', PACKAGE_NAME),
+        'split_by_channel': False,
+        'refresh_interval': 2,
+        'storage_size': 5
+    }
 
     def __init__(self):
         self._generate_path_to_config_file()
@@ -53,22 +76,19 @@ class Config:
                 self.options = _Options(
                     download_path=config_data.get('download_path'),
                     split_by_channel=config_data.get('split_by_channel'),
-                    refresh_interval=config_data.get('refresh_interval')
+                    refresh_interval=config_data.get('refresh_interval'),
+                    storage_size=config_data.get('storage_size')
                 )
         except FileNotFoundError:
-            self._generate_config_file()
+            self._create_config_file()
             self.options = _Options()
         except json.JSONDecodeError:
-            self._generate_config_file()
+            self._create_config_file()
             self.options = _Options()
 
-    def _generate_config_file(self) -> None:
-        home = os.environ.get('HOME')
-        videos_dir = os.path.join(home, 'Videos', PACKAGE_NAME)
-        config = {
-            "download_path": videos_dir,
-            "split_by_channel": False,
-            "refresh_interval": 2
-        }
-        with open(self._config_file_path, 'w') as config_file:
-            json.dump(config, config_file, indent=2)
+    def _create_config_file(self) -> None:
+        try:
+            with open(self._config_file_path, 'w') as config_file:
+                json.dump(self._DEFAULT_CONFIG, config_file, indent=2)
+        except (PermissionError, FileNotFoundError, OSError):
+            pass
