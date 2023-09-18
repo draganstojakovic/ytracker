9cd9672b2d0be76aebbcb42a8e5379943625c7c9
Dragan Stojakovic <stojakovicdragan98@gmail.com>
2023-09-18 22:47:24 +0200

    refactor url_loader

--- ytracker/url_loader.py
+++ ytracker/url_loader.py
@@ -1,64 +1,44 @@
 import os
 import re
-from ytracker.constants import PACKAGE_NAME
-from ytracker.logger import Logger
+from exception import ProgramShouldExit
 
 
 class UrlLoader:
-    __slots__ = '_file_path', '_urls', '_invalid_urls', '_logger'
+    __slots__ = '_urls',
 
-    def __init__(self, file_path: str | None = None):
-        self._logger = Logger()
-        self._urls: list = []
-        self._invalid_urls: list = []
+    def __init__(self, urls: list[str]) -> None:
+        self._urls = urls
+        self.assert_urls()
 
-        if file_path is None:
-            self._file_path: str = os.path.join(
-                os.environ.get('HOME'),
-                '.local',
-                'share',
-                PACKAGE_NAME,
-                'urls.txt'
-            )
-        else:
-            self._file_path = file_path
+    @classmethod
+    def create(cls, package_name: str, file_path: str | None = None) -> 'UrlLoader':
+        file_path = os.path.join(
+            os.path.expanduser('~'),
+            '.local',
+            'share',
+            package_name,
+            f'{package_name}_urls.txt'
+        ) if file_path is None else file_path
 
-        self._set_valid_urls()
-        self._assert_urls()
-        self._assert_invalid_urls()
+        try:
+            with open(file_path, 'r') as file:
+                lines = [line.strip() for line in file.readlines()]
+        except FileNotFoundError:
+            raise ProgramShouldExit(f'File not found: {file_path}', 1)
+        except IOError:
+            raise ProgramShouldExit(f'Failed reading from file: {file_path}', 1)
+
+        return cls([url for url in lines if cls.is_valid_youtube_url(url)])
 
     @classmethod
     def is_valid_youtube_url(cls, url: str) -> bool:
         return re.match(r'https://www\.youtube\.com/@[^/]+', url) is not None
 
     @property
-    def urls(self) -> list[str]:
-        return self._urls
+    def urls(self) -> str:
+        for url in self._urls:
+            yield url
 
-    def _load_lines_from_file(self) -> list[str] | list[None]:
-        try:
-            with open(self._file_path, 'r') as file:
-                return [line.strip() for line in file.readlines()]
-        except FileNotFoundError:
-            self._logger.critical(f'Urls file not found.')
-            raise SystemExit()
-        except IOError:
-            self._logger.critical(f'Error reading file.')
-            raise SystemExit()
-
-    def _set_valid_urls(self) -> None:
-        for url in self._load_lines_from_file():
-            if self.is_valid_youtube_url(url):
-                self._urls.append(url)
-            else:
-                self._invalid_urls.append(url)
-
-    def _assert_urls(self) -> None:
+    def assert_urls(self) -> None:
         if not self._urls:
-            self._logger.critical('None of the urls are valid. Exiting...')
-            raise SystemExit()
-
-    def _assert_invalid_urls(self) -> None:
-        if self._invalid_urls:
-            urls = ', '.join(self._invalid_urls)
-            self._logger.warning(f'Invalid urls: {urls}')
+            raise ProgramShouldExit('Zero valid YouTube urls.', 1)
